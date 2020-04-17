25efd7fc6a35cad8f3dc3f07f7d6a66fbe126f6e
gorgc <1392263019@qq.com>
2020-04-17 17:10:22 +0800

    test

--- k8s/docker_build.sh
+++ k8s/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker_registry=192.168.31.70
-kubectl create secret docker-registry registry-pull-secret --docker-server=$docker_registry --docker-username=admin --docker-password=Harbor12345 --docker-email=admin@ctnrs.com -n ms
+docker_registry=172.16.240.121
+#kubectl create secret docker-registry registry-pull-secret --docker-server=$docker_registry --docker-username=admin --docker-password=Harbor12345 --docker-email=admin@ctnrs.com -n ms
 
 service_list="eureka-service gateway-service order-service product-service stock-service portal-service"
 service_list=${1:-${service_list}}
@@ -20,6 +20,6 @@ for service in $service_list; do
    image_name=$docker_registry/microservice/${service}:$(date +%F-%H-%M-%S)
    docker build -t ${image_name} .
    docker push ${image_name} 
-   sed -i -r "s#(image: )(.*)#\1$image_name#" ${current_dir}/${service}.yaml
-   kubectl apply -f ${current_dir}/${service}.yaml
+   #sed -i -r "s#(image: )(.*)#\1$image_name#" ${current_dir}/${service}.yaml
+   #kubectl apply -f ${current_dir}/${service}.yaml
 done
